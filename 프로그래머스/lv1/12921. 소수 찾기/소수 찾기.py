be12a02965ddc3f61725df1d4a689100f50aec9a
김찬결 <140361642+gyuelgyuel@users.noreply.github.com>
2023-09-25 13:37:29 +0900

    [level 1] Title: 소수 찾기, Time: 1192.29 ms, Memory: 13 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    prime_list = [] # 소수 저장 배열
+    i=2
+    while i<=n:  # 2부터 n까지
+        is_prime = True
+        div_max = int(n**0.5)
+        for j in prime_list:    # i전의 소수들로 나눠보고 나눠지면 소수 아님
+            if j>div_max:
+                break
+            if i%j==0:
+                is_prime=False
+                break
+        if is_prime:            # 소수면 배열에 추가
+            prime_list.append(i)
+        i+=1
+    return len(prime_list)
